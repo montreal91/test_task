340a5d726cff2d6f4827700da65bb9aefc7c7dc0
canadien91 <nefedov.alexander91@yandex.ru>
2013-11-12 12:10:22 +0600

    data models are reworked

--- test_task/models.py
+++ test_task/models.py
@@ -1,23 +1,20 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-class User(models.Model):
-	name = models.CharField(max_length=30)
-	password = models.CharField(max_length=30)
-	cash = models.FloatField(default=0)	
-	is_customer = models.BooleanField(default=False)
-	is_performer = models.BooleanField(default=False)
+class UserUser(User):
+	cash = models.DecimalField(default=0.00, max_digits=20, decimal_places=2)	
 
 	def __unicode__(self):
-		return self.name
+		return self.username
 
 
 class Order(models.Model):
 	title = models.CharField(max_length=200)
 	price = models.FloatField(default=0)
-	customer = models.ForeignKey(User)
-	performer = models.ForeignKey(User)
+	customer = models.ForeignKey(UserUser, related_name='+')
+	performer = models.ForeignKey(UserUser, related_name='+')
 	pub_date = models.DateTimeField('date published')
 
 	def __unicode__(self):
